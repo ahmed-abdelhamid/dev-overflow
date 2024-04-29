c2f727f1643c1396e36e190651a056147c619529
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-04-29 19:41:08 +0300

    add typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,74 @@
+import { Typography } from "@/components/ui/typography";
+
 export default function Home() {
-  return <main className="min-h-screen">DevOverFlow</main>;
+  return (
+    <main className="min-h-screen">
+      <Typography variant="h1" weight="bold">
+        H1-Bold
+      </Typography>
+      <br />
+      <Typography variant="h2" weight="bold">
+        H2-Bold
+      </Typography>
+      <Typography variant="h2" weight="semibold">
+        H2-SemiBold
+      </Typography>
+      <br />
+      <Typography variant="h3" weight="bold">
+        H3-Bold
+      </Typography>
+      <Typography variant="h3" weight="semibold">
+        H3-SemiBold
+      </Typography>
+      <br />
+      <Typography variant="base" weight="medium">
+        Base-Medium
+      </Typography>
+      <Typography variant="base" weight="semibold">
+        Base-SemiBold
+      </Typography>
+      <Typography variant="base" weight="bold">
+        Base-Bold
+      </Typography>
+      <br />
+      <Typography variant="p" weight="regular">
+        Paragraph-Regular
+      </Typography>
+      <Typography variant="p" weight="medium">
+        Paragraph-Medium
+      </Typography>
+      <Typography variant="p" weight="semibold">
+        Paragraph-SemiBold
+      </Typography>
+      <br />
+      <Typography variant="body" weight="regular">
+        Body-Regular
+      </Typography>
+      <Typography variant="body" weight="medium">
+        Body-Medium
+      </Typography>
+      <Typography variant="body" weight="semibold">
+        Body-SemiBold
+      </Typography>
+      <br />
+      <Typography variant="small" weight="regular">
+        Small-Regular
+      </Typography>
+      <Typography variant="small" weight="medium">
+        Small-Medium
+      </Typography>
+      <Typography variant="small" weight="semibold">
+        Small-SemiBold
+      </Typography>
+      <br />
+      <Typography variant="subtle" weight="medium">
+        Subtle-Medium
+      </Typography>
+      <Typography variant="subtle" weight="regular">
+        Subtle-Regular
+      </Typography>
+
+      <Typography>Default</Typography>
+    </main>
+  );
 }
---
+++ components/ui/typography.tsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const typographyVariants = cva("", {
+  variants: {
+    variant: {
+      default: "text-[14px] leading-[19.6px]",
+      h1: "text-[30px] leading-[42px] tracking-[-1px]",
+      h2: "text-[24px] leading-[31.2px]",
+      h3: "text-[20px] leading-[26px]",
+      p: "text-[16px] leading-[22.4px]",
+      body: "text-[14px] leading-[19.6px]",
+      base: "text-[18px] leading-[25.2px]",
+      small: "text-[12px] leading-[15.6px]",
+      subtle: "text-[10px] leading-[13px]",
+    },
+    weight: {
+      default: "font-normal",
+      regular: "font-normal",
+      bold: "font-bold",
+      semibold: "font-semibold",
+      medium: "font-medium",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    weight: "default",
+  },
+});
+
+export interface TypographyProps
+  extends React.HTMLAttributes<HTMLElement>,
+    VariantProps<typeof typographyVariants> {}
+
+const Typography: React.FC<TypographyProps> = ({
+  className,
+  variant,
+  weight,
+  children,
+  ...props
+}) => {
+  if (variant === "h1") {
+    return (
+      <h1
+        className={cn(typographyVariants({ variant, weight, className }))}
+        {...props}
+      >
+        {children}
+      </h1>
+    );
+  }
+
+  if (variant === "h2") {
+    return (
+      <h2
+        className={cn(typographyVariants({ variant, weight, className }))}
+        {...props}
+      >
+        {children}
+      </h2>
+    );
+  }
+
+  if (variant === "h3") {
+    return (
+      <h3
+        className={cn(typographyVariants({ variant, weight, className }))}
+        {...props}
+      >
+        {children}
+      </h3>
+    );
+  }
+
+  if (variant === "p") {
+    return (
+      <p
+        className={cn(typographyVariants({ variant, weight, className }))}
+        {...props}
+      >
+        {children}
+      </p>
+    );
+  }
+
+  return (
+    <div className={cn(typographyVariants({ variant, weight, className }))}>
+      {children}
+    </div>
+  );
+};
+
+Typography.displayName = "Typography";
+
+export { Typography, typographyVariants };
