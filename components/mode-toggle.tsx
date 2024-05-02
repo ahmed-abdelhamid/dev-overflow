508b5aa9706d65e4fdefdf54c37ea0f9eb1b1206
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-05-02 04:39:33 +0300

    add system icon for theme

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { LaptopMinimal, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { cn } from "@/lib/utils";
 import { themes } from "@/constants";
+import { useMounted } from "@/hooks/use-mounted";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -14,16 +15,32 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const ModeToggle = () => {
   const { setTheme, theme } = useTheme();
+  const { mounted } = useMounted();
+
+  if (!mounted)
+    return (
+      <div className="w-10 h-10 flex items-center justify-center">
+        <Skeleton className="w-5 h-5" />
+      </div>
+    );
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 fill-primary-500 stroke-primary-500" />
-          <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:-rotate-90 stroke-none fill-primary-500 dark:scale-100" />
+          {theme === "light" && (
+            <Sun className="h-5 w-5 fill-primary-500 stroke-primary-500" />
+          )}
+          {theme === "dark" && (
+            <Moon className="h-5 w-5 stroke-none fill-primary-500" />
+          )}
+          {theme === "system" && (
+            <LaptopMinimal className="h-5 w-5 fill-primary-500 stroke-primary-500" />
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
