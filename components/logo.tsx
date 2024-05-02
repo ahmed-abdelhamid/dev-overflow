e80f06172800793cce06ca60b384d04574b59fea
ahmed-abdelhamid <slevin.glevra@gmail.com>
2024-05-02 04:48:41 +0300

    make navbar responsive

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import Link from "next/link";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 
 import { useMounted } from "@/hooks/use-mounted";
-import { Skeleton } from "./ui/skeleton";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Logo = () => {
   const { theme } = useTheme();
@@ -12,38 +13,58 @@ const Logo = () => {
 
   if (!mounted)
     return (
-      <div className="flex items-center gap-x-1">
-        <Skeleton className="w-9 h-9" />
-        <Skeleton className="w-[150px] h-8" />
-      </div>
+      <>
+        <div className="items-center gap-x-1 sm:flex hidden">
+          <Skeleton className="w-9 h-9" />
+          <Skeleton className="w-[150px] h-8" />
+        </div>
+
+        <Skeleton className="w-[23px] h-[23px] block sm:hidden" />
+      </>
     );
 
   if (theme === "light") {
     return (
-      <div>
+      <Link href="/">
         <Image
           src="/assets/images/logo-light.svg"
           alt="logo"
           width={186}
           height={36}
-          className="h-[36px]"
+          className="h-[36px] sm:block hidden"
           priority
         />
-      </div>
+        <Image
+          src="/assets/images/site-logo.svg"
+          alt="logo"
+          width={23}
+          height={23}
+          className="h-[23px] block sm:hidden"
+          priority
+        />
+      </Link>
     );
   }
 
   return (
-    <div>
+    <Link href="/">
       <Image
         src="/assets/images/logo-dark.svg"
         alt="logo"
         width={186}
         height={36}
-        className="h-[36px]"
+        className="h-[36px] sm:block hidden"
         priority
       />
-    </div>
+      <Image
+        src="/assets/images/site-logo.svg"
+        alt="logo"
+        width={23}
+        height={23}
+        className="h-[23px] block sm:hidden"
+        priority
+      />
+    </Link>
   );
 };
 export default Logo;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import ModeToggle from "@/components/mode-toggle";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[100px] border-b bg-light-900 dark:bg-dark-200 dark:border-dark-500 dark:shadow-none border-light-800 shadow-light-200 sm:px-12 flex items-center justify-between">
+    <div className="w-full h-[88px] sm:h-[100px] border-b bg-light-900 dark:bg-dark-200 dark:border-dark-500 dark:shadow-none border-light-800 shadow-light-200 sm:px-12 px-8 flex items-center justify-between">
       <Logo />
       <div className="flex items-center gap-x-4">
         <ModeToggle />
